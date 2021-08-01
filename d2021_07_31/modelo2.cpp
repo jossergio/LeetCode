ddd88a1dfcd26262f279234873880331ab20768e
jossergio <jossergio@outlook.com.br>
2021-08-01 08:11:37 -0300

    Dia 31/07/2021

---
+++ d2021_07_31/modelo2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    vector<int> pilha;
+    int posicaoPilha = 0;
+    void iniciarPilha (int t) { pilha.resize (t); } // Pelo máximo
+// = = =
+    int incluirPilha (int v) { // Retorna possível valor acumulado
+        int retorno = 0;
+        if (posicaoPilha == 0) {
+            pilha [posicaoPilha++] = v;
+        } else {
+            if (pilha [posicaoPilha - 1] >= v) {
+                pilha [posicaoPilha++] = v;
+            } else { // if pilha posicaoPilha - 1 >= v
+                int temp = 0;
+                while (temp < posicaoPilha && pilha [temp] > v)
+                    temp++;
+                if (temp == 0) { // 1a posição <= v
+                    for (int temp2 = temp + 1; temp2 < posicaoPilha; temp2++)
+                        retorno += pilha [temp] - pilha [temp2];
+                    posicaoPilha = 0; // Isso
+                } else { // if temp == 0
+                    for (int temp2 = temp; temp2 < posicaoPilha; temp2++) {
+                        retorno += v - pilha [temp2];
+                        pilha [temp2] = v;
+                    } // for temp2 ... (2)
+                } // if temp == 0 ... else
+                pilha [posicaoPilha++] = v;
+            } // if pilha [osicaoPilha] >= v ... else
+        } // if posicaoPilha == 0 ... else
+        return retorno;
+    } // incluirPilha 
+// = = =
+public:
+    int trap(vector<int>& height) {
+        int tamanho = height.size ();
+        int acumulador = 0;
+        iniciarPilha (tamanho);
+        for (int a = 0; a < tamanho; a++)
+            acumulador += incluirPilha (height [a]);
+        return acumulador;
+    }
+};
+
+
