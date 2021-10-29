218e5ab9e9ccb841788e521c422835ec73c34baa
jossergio <jossergio@outlook.com.br>
2021-10-29 18:34:20 -0300

    Dia 26/102021 - Inverter toda uma árvore binária

---
+++ d2021_10_26/modelo.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        TreeNode *tmp;
+        if (root != nullptr) {
+            tmp = root->left;
+            root->left = invertTree (root->right);
+            root->right = invertTree (tmp);
+        }
+    return root;
+    }
+};
+
