d4b4019528994fa2642ac8bca1eb17d21e427203
jossergio <jossergio@outlook.com.br>
2021-11-01 10:37:29 -0300

    Dia 01/11/2021 - Região cercada (não passou nos testes)

---
+++ d2021_11_01/testes.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+)
+
+func temLinhaAntes (board [][] byte, i int, j int) bool {
+	for a := i - 1; a >= 0; a-- {
+		if board [a][j] == 'X' {
+			return true // Basta econtrar o primeiro
+		} // if board a j == 'X'
+	} // for a
+	return false // Se chegou até aqui, não encontrou
+} // temLinhaAntes
+
+func temLinhaDepois (board [][] byte, i int, j int) bool {
+	for a := i + 1; a < len (board); a++ {
+		if board [a][j] == 'X' {
+			return true // Basta econtrar o primeiro
+		} // if board a j == 'X'
+	} // for a
+	return false // Se chegou até aqui, não encontrou
+} // temLinhaDepois
+
+func temColunaAntes (board [][] byte, i int, j int) bool {
+	for a := j - 1; a >= 0; a-- {
+		if board [i][a] == 'X' {
+			return true // Basta econtrar o primeiro
+		} // if board i a == 'X'
+	} // for a
+	return false // Se chegou até aqui, não encontrou
+} // temColunaAntes
+
+func temColunaDepois (board [][] byte, i int, j int) bool {
+	for a := j + 1; a < len (board [i]); a++ {
+		if board [i][a] == 'X' {
+			return true // Basta econtrar o primeiro
+		} // if board i a == 'X'
+	} // for a
+	return false // Se chegou até aqui, não encontrou
+} // temColunaDepois
+
+func temX (board [][] byte, i int, j int) bool {
+	// Optei por dividir em funções especializadas
+	// Para facilitar a compreensão
+	return temLinhaAntes (board, i, j) &&
+		temLinhaDepois (board, i, j) &&
+		temColunaAntes (board, i, j) &&
+		temColunaDepois (board, i, j)
+} // temX
+
+func solve(board [][]byte)  {
+	for i := 0; i < len (board); i++ {
+		for j := 0; j < len (board [i]); j++ {
+			if board [i][j] == 'O' {
+				if temX (board, i, j) {
+					board [i][j] = 'X'
+				} // if temX
+			} // if board [i][j] == 'O'
+		} // for j
+	} // for i
+}
+
+func mostrarBoard (board [][] byte)  {
+	for i := 0; i < len (board); i++ {
+		for j := 0; j < len (board [i]); j++ {
+			fmt.Print (" ", string (board [i][j]))
+		} // for j
+		fmt.Println ()
+	} // for i
+	fmt.Println ()
+} // mostrarBoard
+
+func main() {
+/*	var board [][] byte = [][] byte {{'X', 'X', 'X', 'X'},
+		{'X', 'O', 'O', 'X'},
+		{'X', 'X', 'O', 'X'},
+		{'X', 'O', 'X', 'X'}}
+*/
+	var board [][] byte = [][] byte {{'O', 'X', 'X', 'O', 'X'},
+		{'X', 'O', 'O', 'X', 'O'},
+		{'X', 'O', 'X', 'O', 'X'},
+		{'O', 'X', 'O', 'O', 'O'},
+		{'X', 'X', 'O', 'X', 'O'}}
+
+	mostrarBoard (board)
+	solve (board)
+	mostrarBoard (board)
+
+}
+
