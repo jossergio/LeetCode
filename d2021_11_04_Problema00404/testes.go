eb274046b9ac49d80d076ab9348140e791e3ed38
jossergio <jossergio@outlook.com.br>
2021-11-04 18:55:09 -0300

    Dia 04/11/2021 - Desafio 00404 - Somar folhas esquerdas de uma árvore binária

---
+++ d2021_11_04_Problema00404/testes.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+} // TreeNode struct
+
+func somarFolhasEsquerdas (no *TreeNode, somatorio *int) {
+	if no.Left != nil {
+		filho := no.Left
+		if filho.Left == nil && filho.Right == nil { // Olha só, o filho esquerdo é folha
+			*somatorio += filho.Val
+		} else { // if filho.Left ...
+			somarFolhasEsquerdas (filho, somatorio)
+		} // else if filho.Left
+	}
+	if no.Right != nil { // Não precisa tratar o nó direito. Passa direto.
+		somarFolhasEsquerdas (no.Right, somatorio)
+	} // if no.Right != niol
+} // somarFolhasEsquerdas
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	var somatorio int = 0
+	if root != nil { // Só para não ir nil
+		somarFolhasEsquerdas (root, &somatorio)
+	}
+	return somatorio
+}
+
+func main() {
+	var arvore *TreeNode = &TreeNode {3, &TreeNode {9, nil, nil}, &TreeNode {20, &TreeNode {15, nil, nil}, &TreeNode {7, nil, nil}}}
+	fmt.Println (sumOfLeftLeaves (arvore))
+	arvore = &TreeNode {1, nil, nil}
+	fmt.Println (sumOfLeftLeaves (arvore))
+}
+
