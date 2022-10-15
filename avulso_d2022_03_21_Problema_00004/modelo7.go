89186664d664c457460e794b03c712da6254302f
jossergio <jossergio@outlook.com.br>
2022-10-15 14:32:34 -0300

    Avulso, dia 21/03/2022 - Problema 00004 (atualizado em 15/10/2022) - Média de dois arrays ordenados

---
+++ avulso_d2022_03_21_Problema_00004/modelo7.go
@@ -0,0 +1,34 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    i1, i2 := 0, 0
+    limite := (len (nums1) + len (nums2)) / 2
+    var ant, atual int = 0, 0 // Só para constar
+    var i int = 0
+    passar := func (valor int, proximo *int) {
+        ant = atual
+        atual = valor
+        *proximo += 1
+    }
+    for i <= limite { // 0-based; o último tem que entrar
+        if i1 < len (nums1) && i2 < len (nums2) {
+            if nums1 [i1] <= nums2 [i2] {
+                passar (nums1 [i1], &i1)
+            } else {
+                passar (nums2 [i2], &i2)
+            }
+        } else {
+            fmt.Println (i1, i2)
+            if i1 < len (nums1) {
+                passar (nums1 [i1], &i1)
+            } else {
+                passar (nums2 [i2], &i2)
+            }
+        }
+        i += 1
+    }
+    if (len (nums1) + len (nums2)) % 2 == 0 {
+        return float64 (ant + atual) / 2
+    }
+    // else
+    return float64 (atual)
+}
+
