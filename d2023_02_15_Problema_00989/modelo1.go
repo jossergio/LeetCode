69b01a9d053117fd7b63cbe73fbc37ddf5a39217
jossergio <jossergio@outlook.com.br>
2023-02-15 16:04:08 -0300

    Dia 15/02/2023 - Problema 00989 - Adicionar para formato array de um inteiro

---
+++ d2023_02_15_Problema_00989/modelo1.go
@@ -0,0 +1,64 @@
+func addToArrayForm(num []int, k int) []int {
+    
+    divmod10 := func (a int) (d int, r int) {
+        d = a / 10
+        r = a % 10
+        return // d, r
+    } // divmod10
+    
+    adicionar := func (a int, b int, vemUm int) (soma int, vaiUm int) {
+        var tmp int = a + b + vemUm
+        soma = (tmp % 10)
+        vaiUm = (tmp / 10) % 10
+        return // soma, vaiUm
+    } // vaiUm
+    
+    igualar := func (n [] int, contagem int) (resposta [] int) {
+        for contagem > 0 {
+            resposta = append (resposta, 0)
+            contagem -= 1
+        }
+        return append (resposta, n...)
+    } // igualar
+    
+    inverter := func (n [] int) [] int {
+        var i int = 0
+        var j int = len (n) - 1
+        for i < j {
+            n [i], n [j] = n [j], n [i]
+            i += 1
+            j -= 1
+        }
+        return n
+    }
+    
+    var num2 [] int
+    for k > 0 { // k nunca é zero; definição do problema
+        divisao, resto := divmod10 (k)
+        num2 = append (num2, resto)
+        k = divisao
+    }
+    num2 = inverter (num2)
+    
+    if len (num) > len (num2) {
+        num2 = igualar (num2, len (num) - len (num2))
+    } else {
+        if len (num2) > len (num) { // Sim. Irá dispensar o igual
+            num = igualar (num, len (num2) - len (num))
+        }
+    }
+    
+    var vaiUm int // = 0
+    var adicao int // = 0
+    // adicao definida fora do laço
+    var resposta [] int
+    for i := len (num) - 1; i >= 0; i -= 1 {
+        adicao, vaiUm = adicionar (num [i], num2 [i], vaiUm)
+        resposta = append (resposta, adicao)
+    }
+    if vaiUm == 1 { // Sobrou
+        resposta = append (resposta, 1)
+    }
+    return inverter (resposta) // Vem com os dígitos invertidos
+}
+
---
+++ d2023_02_15_Problema_00989/modelo4.go
@@ -0,0 +1,54 @@
+func addToArrayForm(num []int, k int) []int {
+    
+    divmod10 := func (a int) (d int, r int) {
+        d = a / 10
+        r = a % 10
+        return // d, r
+    } // divmod10
+    
+    adicionar := func (a int, b int, vemUm int) (soma int, vaiUm int) {
+        var tmp int = a + b + vemUm
+        soma = (tmp % 10)
+        vaiUm = (tmp / 10) % 10
+        return // soma, vaiUm
+    } // vaiUm
+    
+    igualar := func (n [] int, contagem int) (resposta [] int) {
+        for contagem > 0 {
+            resposta = append (resposta, 0)
+            contagem -= 1
+        }
+        return append (resposta, n...)
+    } // igualar
+    
+    var num2 [] int
+    for k > 0 { // k nunca é zero; definição do problema
+        divisao, resto := divmod10 (k)
+        num2 = append ([] int {resto}, num2...) // Já monta invertendo
+        k = divisao
+    }
+    
+    if len (num) > len (num2) {
+        num2 = igualar (num2, len (num) - len (num2))
+    } else {
+        if len (num2) > len (num) { // Sim. Irá dispensar o igual
+            num = igualar (num, len (num2) - len (num))
+        }
+    }
+    
+    resposta := make ([] int, len (num)) // Aqui, tanto faz o vetor para tamanho
+    for i := len (num) - 1; i >= 0; i -= 1 {
+        adicao, vaiUm := adicionar (num [i], num2 [i], resposta [i])
+        resposta [i] = adicao
+        if i > 0 {
+            resposta [i - 1] = vaiUm
+        } else {
+            if vaiUm == 1 {
+                resposta = append ([] int {1}, resposta...) // Adiciona um possível vaiUm final
+            }
+        }
+        
+    }
+    return resposta // Vem com os dígitos invertidos
+}
+
