1371ed4ed177d12e78523617dc61500f1cdc3b46
jossergio <jossergio@outlook.com.br>
2023-05-03 16:07:46 -0300

    Dia 03/05/2023 - Problema 02215 - Encontrar a diferença entre dois vetores

---
+++ d2023_05_03_Problema_02215/modelo2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> resposta (2, vector<int> ());
+        set<int> conjunto1 (nums1.begin (), nums1.end ()); // Tornam únicos os elementos
+        set<int> conjunto2 (nums2.begin (), nums2.end ()); // Idem
+        auto i = conjunto1.begin (); // O tipo é um iterator
+        auto j = conjunto2.begin (); // Idem
+        while (i != conjunto1.end () && j != conjunto2.end ()) {
+            if (*i == *j) {
+                i++;
+                j++;
+            } else {
+                if (*i < *j) {
+                    resposta [0].push_back (*i);
+                    i++;
+                } else { // *j > *i
+                    resposta [1].push_back (*j);
+                    j++;
+                }
+            }
+        }
+        while (i != conjunto1.end ()) {
+            resposta [0].push_back (*i);
+            i++;
+        }
+        while (j != conjunto2.end ()) {
+            resposta [1].push_back (*j);
+            j++;
+        }
+        return resposta;
+    }
+};
+
