fb534e2faf08b939f6de6c3a19405bcc675be2c1
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2023-10-27 17:36:05 -0300

    Dia 27/10/2023 - Problema 00005 - Mais longa substring palíndromo

---
+++ d2023_10_27_Problema_00005/modelo.go
@@ -0,0 +1,50 @@
+func maiorPalindromoLocal (s string, i int, f int) (inicio int, final int) {
+    // Retorna, a partir de uma base central, o maior palíndromo
+    // dentro dos limites da string
+    
+    inicio = i
+    final = f
+    
+    for (inicio >= 0 && final < len (s)) && (s [inicio] == s [final]) {
+        inicio--
+        final++
+    }
+    
+    if (inicio >= 0 && final < len (s)) {
+        if (s [inicio] != s [final]) {
+            inicio += 1
+            final -= 1
+        }
+    } else {
+        inicio += 1
+        final -= 1
+    }
+    
+    return // inicio, final
+}
+
+func longestPalindrome(s string) string {
+    
+    var rInicio, rFinal int // = 0, 0
+    
+    // Temporárias no laço
+    var inicio, final int // = 0, 0
+    
+    for i := 0; i < len (s); i++ {
+        
+        inicio, final = maiorPalindromoLocal (s, i, i) // Para palíndromos ímpares
+        
+        if (final - inicio) > (rFinal - rInicio) {
+            rInicio, rFinal = inicio, final
+        }
+        
+        // Segunda passada, para palídromos pares
+        inicio, final := maiorPalindromoLocal (s, i, i + 1)
+        if (final - inicio) > (rFinal - rInicio) {
+            rInicio, rFinal = inicio, final
+        }
+    }
+    
+    return s [rInicio:rFinal + 1] // O último não entraria
+}
+
