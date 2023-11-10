ee7f0d3ae90e62a47aa7166ff280cb62dc150084
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2023-11-10 15:47:46 -0300

    Dia 10/11/2023 - Problema 01743 - Restaurar um vetor a partir de pares adjacentes

---
+++ d2023_11_10_Problema_01743/modelo1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        map<int, vector<int>> grafo; // Só para mostrar a ideia que tive
+        set<int> unicos; // Ao final do laço abaixo, só pode ter dois: os possíveis primeiros e últimos
+        set<int> utilizados;
+        for (auto a: adjacentPairs) {
+            grafo [a [0]].push_back (a [1]);
+            grafo [a [1]].push_back (a [0]);
+            if (unicos.count (a [0]) > 0)
+                unicos.erase (a [0]); // É a segunda vez que aparece; retirar
+            else
+                unicos.insert (a [0]);
+            if (unicos.count (a [1]) > 0)
+                unicos.erase (a [1]);
+            else
+                unicos.insert (a [1]);
+            utilizados.insert (a [0]);
+            utilizados.insert (a [1]);
+        }
+        int t = adjacentPairs.size () + 1;
+        int atual = *unicos.begin ();
+        vector<int> resposta (t);
+        resposta [0] = atual;
+        utilizados.erase (atual);
+        atual = grafo [atual][0]; // Esse, só pode ter um
+        for (int i = 1; i < t - 1; i += 1) {
+            resposta [i] = atual;
+            utilizados.erase (atual);
+            if (utilizados.count (grafo [atual][0]) > 0)
+                atual = grafo [atual][0];
+            else
+                atual = grafo [atual][1];
+        }
+        resposta [t - 1] = atual; // O que sobrou
+        return resposta;
+    }
+};
+
