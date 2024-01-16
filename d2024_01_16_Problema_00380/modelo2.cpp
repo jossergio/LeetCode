bf8a402c14671e807c13679b95b832ab4eea8902
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-01-16 14:27:06 -0300

    Dia 16/01/2024 - Problema 00380 - Inserir remover pegar aleatório O(1)

---
+++ d2024_01_16_Problema_00380/modelo2.cpp
@@ -0,0 +1,41 @@
+class RandomizedSet {
+private:
+    set<int> c;
+public:
+    RandomizedSet() {
+        // Nada a fazer
+    }
+    
+    bool insert(int val) {
+        if (c.count (val) == 0) {
+            c.insert (val);
+            return true;
+        }
+        // else
+        return false;
+    }
+    
+    bool remove(int val) {
+        if (c.count (val) != 0) {
+            c.erase (val);
+            return true;
+        }
+        // else
+        return false;
+    }
+    
+    int getRandom() {
+        vector<int> v (c.begin (), c.end ());
+        int i = rand () % c.size (); // Só para não gerar confusão, borrando o código
+        return v [i];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+ 
