69536f1ef4c53e181ce5f7a63efcc4c50fee7846
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-04-29 19:47:19 -0300

    Dia 29/04/2024 - Problema 02997 - Menor número de operações para fazer um array XOR igual a K

---
+++ d2024_04_29_Problema_02997/modelo1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int acumulado = 0;
+        for (int i: nums)
+            acumulado =  acumulado xor i;
+        int base = 1; // Deslocara shift left, para comparar cada bit no laço abaixo
+        int contagem = 0;
+        while (acumulado != k) {
+            if ((acumulado & base) != (k & base)) {
+                contagem += 1;
+                acumulado = acumulado xor base;
+            }
+            base <<= 1;
+        }
+        return contagem;
+    }
+};
+
