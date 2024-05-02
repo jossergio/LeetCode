4aa42a2f81669e77b21f5770699f3ab19183a194
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-05-02 13:05:08 -0300

    Dia 02/05/2024 - Problema 02441 - Maior inteiro positivo que há com seu negativo

---
+++ d2024_05_02_Problema_02441/modelo2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort (nums.begin (), nums.end());
+        int i = 0;
+        int j = nums.size () - 1;
+        while (i < j + 1) {
+            if (nums [j] < 0 || nums [i] > 0)
+                return -1; // Nem tem como tentar mais
+            if (-nums [i] == abs (nums [j]))
+                return nums [j]; // É esse
+            if (-nums [i] > nums [j])
+                i += 1; // Segue para o próximo negativo
+            else // Só pode ser menor
+                j -= 1; // Proximo menor positivo
+        }
+        return -1; // Passou por tudo e não encontrou
+    }
+};
+
