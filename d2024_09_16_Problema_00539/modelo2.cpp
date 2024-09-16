64b933c55f2411f59f6aa9b31469a615987bb154
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-09-16 20:20:53 -0300

    Dia 16/09/2024 - Problema 00539 - Menor diferença de tempo

---
+++ d2024_09_16_Problema_00539/modelo2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int paraMinutos (string t) {
+        int h = stoi (t.substr (0, 2));
+        int m = stoi (t.substr (3));
+        if (h == 0) h = 24;
+        return 60 * h + m;
+    }
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        const size_t t = timePoints.size ();
+        vector<int> minutos;
+        minutos.reserve (t);
+        for (string s: timePoints) {
+            minutos.push_back (paraMinutos (s));
+        }
+        sort (minutos.begin (), minutos.end ());
+        int resposta = abs (minutos [t - 1] - 24 * 60 - minutos [0]); 
+        for (int i = 1; i < t; i += 1) {
+            resposta = min (resposta, minutos [i] - minutos [i - 1]);
+        }
+        return resposta;
+    }
+};
+
