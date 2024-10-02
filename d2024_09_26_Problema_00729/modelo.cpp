5150d4938735bcf4b4554575a8b6e774543e0120
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-10-02 15:59:17 -0300

    Dia 26/09/2024 - Prolema 00729 - Meu Calendário I

---
+++ d2024_09_26_Problema_00729/modelo.cpp
@@ -0,0 +1,28 @@
+class MyCalendar {
+private:
+    vector<pair<int, int>> lista;
+    
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+        for (auto p = lista.begin (); p != lista.end (); p++)
+            if (p->second > start && p->first < end)
+                return false;
+        // else
+        
+        lista.push_back ({start, end});
+        
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
+ 
