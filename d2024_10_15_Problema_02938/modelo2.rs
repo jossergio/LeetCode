3713da5eed9a7da2a54d7bb6655b49ef2dab9dbc
Jossérgio Soares Antas de Gouveia <jossergio@outlook.com.br>
2024-10-15 14:05:02 -0300

    Dia 15/10/2024 - Problema 02938 - Separar bolas pretas e brancas

---
+++ d2024_10_15_Problema_02938/modelo2.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn minimum_steps(s: String) -> i64 {
+        let mut d: Vec<u8> = s.bytes ().collect ();
+        let mut contador: i64 = 0;
+        let mut permuta = true;
+        let mut zero: i32 = d.len () as i32 - 1; // Onde deve ficar o último algarismo 0 reversamente
+        let mut um: i32 = zero; // Só para ter um valor talvez maior e dentro do limite de "zero"
+        // Obs.: i32, pois -1 indica que terminou
+        while permuta {
+            while zero >= 0 && d [zero as usize] != b'0' {
+                zero -= 1;
+            }
+            if um > zero {
+                um = zero - 1;
+            }
+            while um >= 0 && d [um as usize] != b'1' {
+                um -= 1;
+            }
+            if um >= 0 { // Basta checar esse, pois veio por derivação do outro
+                d [zero as usize] = b'1';
+                d [um as usize] = b'0';
+                contador += (zero - um) as i64; // A quantidade de permutas para ajeitar esse par
+                um -= 1; // Para fazer busca a partir daqui, na próxima iteração
+            } else {
+                permuta = false; // Não há mais necessidade de permutar nada
+            }
+        }
+        contador
+    }
+}
+
