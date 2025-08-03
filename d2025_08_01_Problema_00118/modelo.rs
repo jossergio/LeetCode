b8648d82f0b741420d54e4754a6d741025be1083
jossergio <jossergio@outlook.com.br>
2025-08-03 18:53:40 -0300

    Dia 01/08/2025 - Problema 00118 - Triângulo de Pascal

---
+++ d2025_08_01_Problema_00118/modelo.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut resposta: Vec<Vec<i32>> = vec! [vec! [1; 1]; num_rows as usize]; // Já adiciona a primeira linha
+        for i in 1..num_rows as usize { // Será utilizada para índice de slice
+            for j in 1..i { // Infere o tipo a patir de i
+                let tmp: i32 = resposta [i - 1][j] + resposta [i - 1][j - 1]; // Necessário, por conta de empréstimos (borrows)
+                resposta [i].push (tmp);
+            }
+            resposta [i].push (1);
+        }
+        resposta
+    }
+}
