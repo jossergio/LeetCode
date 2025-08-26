9ca8bcc320c80bf8d5b9f757b5aeb274880a19f8
jossergio <jossergio@outlook.com.br>
2025-08-26 19:47:29 -0300

    Dia 26/08/2025 - Problema 03000 - Área máxima de retâgulo com a maior diagonal

---
+++ d2025_08_26_Problema_03000/modelo1.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn area_of_max_diagonal(dimensions: Vec<Vec<i32>>) -> i32 {
+        let mut m_diag = 0f64;
+        let mut resposta: i32 = 0;
+        for d in dimensions {
+            let tmp = f64::from (d [0] * d [0] + d [1] * d [1]).sqrt ();
+            if tmp > m_diag {
+                m_diag = tmp;
+                resposta = d [0] * d [1];
+            } else if tmp == m_diag { // Se iguais, pega o de maior área
+                let tmp_area = d [0] * d [1];
+                if tmp_area > resposta {
+                    resposta = tmp_area;
+                }
+            }
+        }
+        resposta
+    }
+}
