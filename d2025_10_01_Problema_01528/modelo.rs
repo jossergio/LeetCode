1bd44d618d5e12c980303d3a519678d3d67763cc
jossergio <jossergio@outlook.com.br>
2025-10-02 12:22:44 -0300

    Dia 01/10/2025 - Problema 01518 - Garrafas de água

---
+++ d2025_10_01_Problema_01528/modelo.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn num_water_bottles(num_bottles: i32, num_exchange: i32) -> i32 {
+        let mut resposta: i32 = 0;
+        let mut vazias: i32 = 0;
+        let mut num_bottles: i32 = num_bottles; // Só para "sombrear"
+        while (num_bottles > 0) {
+            resposta += num_bottles;
+            vazias += num_bottles;
+            num_bottles = vazias / num_exchange;
+            vazias %= num_exchange;
+        }
+        return resposta;
+    }
+}
