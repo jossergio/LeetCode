854508f3e6aa3da1936df236a7d1074e263b8964
jossergio <jossergio@outlook.com.br>
2025-10-19 19:01:13 -0300

    Dia 13/10/2025 - Problema 02273 (atualização em 19/10/2025 - Encontrar o vetor resultante após remover anagramas

---
+++ d2025_10_13_Problema_02273/modelo3.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn remove_anagrams(words: Vec<String>) -> Vec<String> {
+        let ordenados = words.iter ()
+            .map (| w | {
+                let mut tmp = w.chars ().collect::<Vec<_>> ();
+                tmp.sort ();
+                tmp
+            }).collect::<Vec<_>> ();
+        let mut ficam = vec![0 as usize]; // O 1o sempre fica
+        for i in 1..words.len () {
+            println! ("{:?} {:?}", ordenados [i], ordenados [i - 1]);
+            if ordenados [i] != ordenados [i - 1] {
+                ficam.push (i);
+            }
+        }
+        ficam.into_iter ().map (| i | &words [i]).map (| s | s.clone ()).collect::<Vec<_>> ()
+    }
+}
