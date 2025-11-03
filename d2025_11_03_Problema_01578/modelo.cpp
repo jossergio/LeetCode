80b0703c2694161df70de677860aa9275248125f
jossergio <jossergio@outlook.com.br>
2025-11-03 18:50:49 -0300

    Dia 03/11/2025 - Problema 01578 - Tempo mínimo para colorir uma corda

---
+++ d2025_11_03_Problema_01578/modelo.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        const int t = colors.size (); // Tanto faz, são do mesmo tamanho
+        int i = 0;
+        int tmpCor, anterior; // Serão utilizados no laço
+        int resposta = 0;
+        while (i < t) {
+            tmpCor = colors [i];
+            anterior = neededTime [i];
+            i += 1;
+            while (i < t && colors [i] == tmpCor) {
+                if (neededTime [i] < anterior) {
+                    resposta += neededTime [i];
+                } else {
+                    resposta += anterior;
+                    anterior = neededTime [i];
+                }
+                i += 1;
+            }
+            // O contador já foi incrementado
+        }
+        return resposta;
+    }
+};
