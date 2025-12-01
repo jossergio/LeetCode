a3741e570d723c2faa6a0ea6e24b2a6c7125482f
jossergio <jossergio@outlook.com.br>
2025-12-01 15:16:08 -0300

    Dia 01/12/2025 - Problema 02141 - Tempo máximo de execução de n computadores

---
+++ d2025_12_01_Problema_02141/modelo.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        sort (batteries.begin (), batteries.end ());
+        long extra = 0;
+        for (int i = 0; i < batteries.size () - n; i += 1)
+            extra += batteries [i];
+        vector<int> uso = vector (batteries.end () - n, batteries.end ());
+        for (int i = 0; i < n - 1; i += 1) {
+            if (extra < (long) ((i + 1) * (uso [i + 1] - uso [i])))
+                return uso [i] + (extra / ((long) (i + 1)));
+            extra -= (long) ((i + 1) * (uso [i + 1] - uso [i]));
+        }
+        return uso [n - 1] + (extra / n);
+    }
+};
