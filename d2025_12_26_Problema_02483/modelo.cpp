4b5f5895d9907ceb302e435a6e35eef651dfb886
jossergio <jossergio@outlook.com.br>
2025-12-27 15:36:35 -0300

    Dia 26/12/2025 - Problema 02483 - Menor penalidade para uma compra

---
+++ d2025_12_26_Problema_02483/modelo.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int atual = 0;
+        int menor = 0; // Pressupõe
+        int resposta = 0; // Posição inicial
+        int i = 1; // Posição atual, segunda casa
+        for (char c: customers) {
+            if (c == 'Y')
+                menor -= 1;
+            else
+                menor += 1;
+            if (menor < atual) { // Igual, pois o menor tem preferência
+                atual = menor;
+                resposta = i;
+            }
+            i += 1;
+        }
+        return resposta;
+    }
+};
