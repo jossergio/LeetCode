e989ed1343683e386b1832a1c049e9ea9336c440
jossergio <jossergio@outlook.com.br>
2026-01-22 14:39:52 -0300

    Dia 21/01/2026 - Problema 03315 - Construir o vetor bit a bit mínimo II

---
+++ d2026_01_21_Problema_03315/modelo1.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
+        nums
+            .iter ()
+            .enumerate ()
+            .map (| (i, v) | {
+                let mut n = -1; // Padrão
+                let mut d = 1; // Diferença
+                while v & d != 0 {
+                    n = v - d;
+                    d <<= 1;
+                }
+                n
+            }).collect::<Vec<_>> ()
+    }
+}
---
+++ d2026_01_21_Problema_03315/modelo2.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
+        nums
+            .into_iter ()
+            .map (| v | {
+                let mut n = -1; // Padrão
+                let mut d = 1; // Diferença
+                while v & d != 0 {
+                    n = v - d;
+                    d <<= 1;
+                }
+                n
+            }).collect::<Vec<_>> ()
+    }
+}
