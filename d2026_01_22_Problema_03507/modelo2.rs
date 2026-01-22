1ceb8668d4afdc53534a83fe8f48f45b8d82cfef
jossergio <jossergio@outlook.com.br>
2026-01-22 15:08:22 -0300

    Dia 22/01/2026 - Problema 03507 - Menor remoção de pares para ordenar um vetor I

---
+++ d2026_01_22_Problema_03507/modelo2.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn minimum_pair_removal(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let mut resposta = 0; // i32
+        loop {
+            if nums.len () == 1 { break; } // Não há mais o que fazer
+            // else
+            let mut menor = nums [0] + nums [1]; // Base
+            let mut menor_i: usize = 0;
+            let mut tem = false;
+            for i in 1..nums.len () {
+                tem = tem || (nums [i] < nums [i - 1]);
+                let tmp = nums [i] + nums [i - 1];
+                if tmp < menor {
+                    menor = tmp;
+                    menor_i = i - 1; // Toma como base esse
+                }
+            }
+            if !tem { break; }
+            // else
+            nums.remove (menor_i);
+            nums [menor_i] = menor; // No que foi puxado, que era o par
+            resposta += 1;
+        }
+        resposta
+    }
+}
