732ef9cf80a12bab7032699ddd7dbedb308afd63
jossergio <jossergio@outlook.com.br>
2026-02-10 18:48:08 -0300

    Dia 09/02/2026 - Problema 01382 - Balancear uma árvore binária de busca

---
+++ d2026_02_09_Problema_01382/modelo.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void emOrdem (vector<int>& v, TreeNode* no) {
+        if (no != nullptr) {
+            emOrdem (v, no->left);
+            v.push_back (no->val);
+            emOrdem (v, no->right);
+        }
+    }
+    TreeNode* leitor (vector<int>& v, int i, int j) {
+        if (i > j)
+            return nullptr;
+        // else
+        int meio = (i + j) / 2;
+        return new TreeNode (v [meio], leitor (v, i, meio - 1), leitor (v, meio + 1, j));
+    }
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> v;
+        emOrdem (v, root);
+        return leitor (v, 0, v.size () - 1);
+    }
+};
