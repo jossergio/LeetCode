10bfdcee0b0757f3512c6e531319b3ced1cc9e79
jossergio <jossergio@outlook.com.br>
2026-02-24 17:41:29 -0300

    Dia 24/02/2026 - Problema 01022 - Somar raiz para folhas números binários em uma árvore binária

---
+++ d2026_02_24_problema_01022/modelo.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func soma (no *TreeNode, subtotal int) int {
+    if no != nil {
+        subtotal <<= 1
+        subtotal += no.Val
+        if no.Left == nil && no.Right == nil {
+            return subtotal
+        } else {
+            return soma (no.Left, subtotal) + soma (no.Right, subtotal)
+        }
+    }
+    return 0 // Só se a Árvore for vazia
+}
+
+func sumRootToLeaf(root *TreeNode) int {
+    return soma (root, 0)
+}
