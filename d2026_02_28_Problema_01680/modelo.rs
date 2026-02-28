c2d891f0aae6c2a36857a87b06be62f4e47e537e
jossergio <jossergio@outlook.com.br>
2026-02-28 14:25:46 -0300

    Dia 28/02/2026 - Problema 01680 - Concatenação de números binários consecutivos

---
+++ d2026_02_28_Problema_01680/modelo.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn concatenated_binary(n: i32) -> i32 {
+        
+        let MODULO:i64 = 1000000007;
+        let mut acumulado:i64 = 0;
+        
+        for i in 1..=n as i64 {
+            
+            let mut deslocamento:i64 = i; // Desloca com base no bit 1 mais significativo do valor que vai entrar, para dar espao
+            
+            while deslocamento > 0 {
+                acumulado <<= 1; // Desloca para a esquerda
+                deslocamento >>= 1; // Desloca para a direita
+            }
+            
+            acumulado += i;
+            acumulado %= MODULO;
+        }
+        
+        return acumulado as i32
+    }
+}
