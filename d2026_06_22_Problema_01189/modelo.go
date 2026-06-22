f24b480794ea734f07abe57cfb588c2af9ff671c
jossergio <jossergio@outlook.com.br>
2026-06-22 18:06:48 -0300

    Dia 22/06/2026 - Problema 01189 - Número máximo de ballons (letras que formam a palavra em uma string)

---
+++ d2026_06_22_Problema_01189/modelo.go
@@ -0,0 +1,17 @@
+func maxNumberOfBalloons(text string) int {
+	var maximo int = 2000
+	for _, letra := range "balon" {
+		valor := strings.Count (text, string (letra))
+		if letra == 'l' || letra == 'o' {
+			valor /= 2
+		} // if letra...
+		if valor == 0 {
+			return 0 // Nem precisa mais
+		} else { // if valor == 0
+			if valor < maximo {
+				maximo = valor
+			} // if valor < maximo
+		} // else if valor == 0
+	}
+	return maximo
+}
