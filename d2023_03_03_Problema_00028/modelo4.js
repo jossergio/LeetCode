3b231e7dcba79130e1cabd3950651885fab13178
jossergio <jossergio@outlook.com.br>
2026-07-27 15:50:26 -0300

    ia 03/03/2023 - Problema 00028 (atualização em 27/07/2026) - Encontrar o índice da primeira ocorrência em uma string

---
+++ d2023_03_03_Problema_00028/modelo4.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let th = haystack.length;
+    let tn = needle.length;
+    if (tn <= th) {
+        for (let i = 0; i < th - tn + 1; i += 1) {
+            if (needle === haystack.substring (i, i + tn)) {
+                return i;
+            }
+        }
+    }
+    return -1; // Não achou
+};
