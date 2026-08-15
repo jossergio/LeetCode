8509f03389681798a727edcf267700f0525c7c02
jossergio <jossergio@outlook.com.br>
2026-08-15 11:16:34 -0300

    Dia 14/08/2026 - Problema 03090 - Comprimento máximo de substring com duas ocorrências

---
+++ d2026_08_14_Problema_03090/modelo2.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maximumLengthSubstring = function(s) {
+    let mapa = new Map ();
+    let resposta = 0;
+    let i = 0;
+    for (let j = 0; j < s.length; j += 1) {
+        let c = s [j]; // Facilita visualização
+        if (mapa.has (c)) {
+            let tmp = mapa.get (c);
+            if (tmp.length === 2) {
+                if (tmp [0] >= i) {
+                    i = tmp [0] + 1;
+                }
+                tmp [0] = tmp [1];
+                tmp [1] = j;
+            } else { // Só pode ser 1
+                tmp [1] = j; // Adiciona
+            }
+            mapa.set (c, tmp);
+        } else {
+            mapa.set (c, [j]);
+        }
+        resposta = Math.max (resposta, j - i + 1);
+    }
+    return resposta;
+};
